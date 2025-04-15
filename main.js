110a11590fc4fd5d326f7b63b339d5e847448a77
Jean Carlos I. Stricagnolo Soares <jean.soares@ext.sistemafiep.org.br>
2025-04-15 19:40:30 -0300

    realizado a criação de verificação se o email foi colocado da forma correta

---
+++ main.js
@@ -0,0 +1,10 @@
+//configura acesso para a tela de login
+function acessar(){
+    let loginEmail = document.getElementBYId('loginEmail').value;
+    let loginSenha = document.getElementBYId('loginEmail').value;
+
+
+
+    //implementa estrutura que testa se todos os campos foram preenchidos 
+    
+}
